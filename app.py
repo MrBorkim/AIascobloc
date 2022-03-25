c93c8830f72685220626b56f97d6a17c36a20da0
MrBorkim <38116784+MrBorkim@users.noreply.github.com>
2022-03-25 17:48:39 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,66 @@
+from pycaret.regression import load_model, predict_model
+import streamlit as st
+import pandas as pd
+import numpy as np
+import xgboost
+
+model = load_model('final xgboost')
+
+def predict(model, input_df):
+    predictions_df = predict_model(estimator=model, data=input_df)
+    predictions = predictions_df['Label'][0]
+    return predictions
+
+def run():
+
+    from PIL import Image
+    image = Image.open('logo.png')
+    image_hospital = Image.open('hospital.jpg')
+
+    st.image(image,use_column_width=False)
+
+    add_selectbox = st.sidebar.selectbox(
+    "How would you like to predict?",
+    ("Online", "Batch"))
+
+    st.sidebar.info('This app is created to predict patient hospital charges')
+    st.sidebar.success('https://www.pycaret.org')
+    
+    st.sidebar.image(image_hospital)
+
+    st.title("Insurance Charges Prediction App")
+
+    if add_selectbox == 'Online':
+
+        D = st.number_input('Długość', min_value=100, max_value=3000, value=1200)
+        G = st.number_input('Głębokość', min_value=100, max_value=3000, value=600)
+        W = st.number_input('Wysokość', min_value=50, max_value=2500, value=850)
+        
+        if st.checkbox('Standard'):
+            smoker = 'yes'
+        else:
+            smoker = 'no'
+        Typ = st.selectbox('Typ katalogowy', ['A3100.126', 'A3100.128', 'A3101.126', 'A3105.126'])
+
+        output=""
+
+        input_dict = {'Długość' : D, 'Głębokość' : G, 'Wysokość' : W,  'Typ katalogowy' : Typ}
+        input_df = pd.DataFrame([input_dict])
+
+        if st.button("Predict"):
+            output = predict(model=model, input_df=input_df)
+            output = '$' + str(output)
+
+        st.success('The output is {}'.format(output))
+
+    if add_selectbox == 'Batch':
+
+        file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
+
+        if file_upload is not None:
+            data = pd.read_csv(file_upload)
+            predictions = predict_model(estimator=model,data=data)
+            st.write(predictions)
+
+if __name__ == '__main__':
+    run()
